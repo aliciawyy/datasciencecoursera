262cefc324d7ef171e746497033ab932ec0b3be6
Alice Wang <rainingilove@gmail.com>
2016-10-02 17:42:55 +0200

    test adding scikit learn in requirements

--- core/struct.py
+++ core/struct.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sklearn import cross_validation
 import dm_common
 
 
@@ -24,3 +24,11 @@ class Problem(dm_common.StringMixin):
     @property
     def target(self):
         return self._target
+
+    def train_test_split(self, test_size=0.2, random_state=0):
+        x_train, x_test, y_train, y_test = cross_validation.train_test_split(
+            self.data, self.target, test_size=test_size, random_state=random_state)
+        problem_train = Problem(x_train, y_train)
+        problem_test = Problem(x_test, y_test)
+        return problem_train, problem_test
+
