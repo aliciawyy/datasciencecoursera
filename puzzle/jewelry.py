704f0b17a9421df6cb947e67d13f77fb4afb3c0a
Alice Wang <rainingilove@gmail.com>
2017-03-19 17:39:41 +0100

    simplify the solution of jewelry

--- puzzle/jewelry.py
+++ puzzle/jewelry.py
@@ -3,23 +3,24 @@ from scipy.special import comb
 import numpy as np
 
 
-def _iter_sorted_dct(dct):
+def _iter_key_sorted_dct(dct):
     for k in sorted(dct.keys()):
         yield k, dct[k]
 
 
 def make_sum(dct_values, base=None):
+    """base is some previous result"""
     sum_cnt = collections.defaultdict(int)
     if base is not None:
         sum_cnt.update(base)
-    for v, n in _iter_sorted_dct(dct_values):
+    for v, n in _iter_key_sorted_dct(dct_values):
         # to include from 1 to n elements of value v
         dct = dict(sum_cnt)
         for i in range(1, n + 1):
             n_ways = comb(n, i)
             increment = i * v  # increment for sum by including n times v
             sum_cnt[increment] += n_ways
-            for k, v_orig in _iter_sorted_dct(dct):
+            for k, v_orig in _iter_key_sorted_dct(dct):
                 sum_cnt[k + increment] += n_ways * v_orig
     return sum_cnt
 
@@ -44,37 +45,23 @@ class Jewelry(object):
         count = 0
         values_for_above = dict(self.values_)
 
-        for v, cnt in _iter_sorted_dct(self.values_):
+        for v, cnt in _iter_key_sorted_dct(self.values_):
+            # Remove value v iteratively to get all the possible sums from
+            # the values above v
             values_for_above.pop(v)
             ways_above_exclude_v = make_sum(values_for_above)
             ways_below_exclude_v = dict(self.ways_below_)
             for i in range(1, cnt + 1):
                 n_ways = comb(cnt, i)
                 ways_below = collections.defaultdict(int)
-                for k, v_orig in _iter_sorted_dct(ways_below_exclude_v):
+                for k, cnt_orig in _iter_key_sorted_dct(ways_below_exclude_v):
                     sum_with_iv = k + v * i
-                    increment = n_ways * v_orig
-                    ways_below[sum_with_iv] += increment
-                    self.ways_below_[sum_with_iv] += increment
-                # The ways above can include n - i elements in maximum
-                m = cnt - i
-                # Count the possible sum from elements above exclude v and
-                # with from 1 to n - i elements of v
-                ways_above = collections.defaultdict(int)
-                ways_above.update(ways_above_exclude_v)
-                for j in range(1, m + 1):
-                    n_ways = comb(m, j)
-                    value_increment = v * j
-                    ways_above[value_increment] += n_ways
-                    for k, v_orig in _iter_sorted_dct(ways_above_exclude_v):
-                        ways_above[k + value_increment] += n_ways * v_orig
+                    cnt_increment = n_ways * cnt_orig
+                    ways_below[sum_with_iv] += cnt_increment
+                    self.ways_below_[sum_with_iv] += cnt_increment
+                # The ways above can include cnt - i elements in maximum
+                ways_above = make_sum({v: cnt - i}, ways_above_exclude_v)
                 intersection = set(ways_below).intersection(ways_above)
                 count += np.sum([ways_below[k] * ways_above[k]
                                  for k in intersection])
         return count
-
-
-
-
-
-
