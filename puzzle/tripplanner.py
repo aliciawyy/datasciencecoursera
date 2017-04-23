0c1b06d5506ea6d125292f3c7aeb6f34031e1d4e
Alice Wang <rainingilove@gmail.com>
2017-04-23 15:07:22 +0200

    simplified trip planner

--- puzzle/tripplanner.py
+++ puzzle/tripplanner.py
@@ -1,10 +1,7 @@
-import collections
-
-
-def read_line_to_list(as_type=int):
+def read_line_to_list(as_type=int, start=0):
     lst = raw_input().strip().split(' ')
-    if as_type is None:
-        return lst
+    if start > 0:
+        lst = lst[start:]
     return map(as_type, lst)
 
 
@@ -15,36 +12,30 @@ class TripPlanner(object):
         self.budget = budget
         self.price_per_day = price_per_day
         self.num_attractions = num_attr
-        self.attractions = [(v, c, s) for c, v, s in attractions]
-        self.attractions = sorted(self.attractions)
+        self.attractions = attractions
 
     def max_score_sum(self):
-        max_sum = collections.defaultdict(int)
-        max_sum[(self.budget, 0)] = 0
+        max_sum = {(self.budget, 0): 0}
 
-        max_sum_of_all = 0
-        for visit_time, cost, score in self.attractions:
-            new_max_sum = collections.defaultdict(int)
+        for cost, visit_time, score in self.attractions:
+            new_max_sum = dict(max_sum)
             # print "\n", max_sum
             for (budget, time_cnt), score_sum in max_sum.items():
-                day_cnt, past_time = divmod(time_cnt, self.hours_per_day)
+                past_time = time_cnt % self.hours_per_day
                 remaining_budget = budget - cost
                 if past_time == 0 or visit_time > self.hours_per_day - past_time:
                     remaining_budget -= self.price_per_day
 
-                if remaining_budget < 0:
-                    continue
-                new_key = (remaining_budget, time_cnt + visit_time)
-                this_max_sum = max(max_sum[new_key], score_sum + score,
-                                   new_max_sum[new_key])
-                new_max_sum[new_key] = this_max_sum
-                max_sum_of_all = max(max_sum_of_all, this_max_sum)
+                if remaining_budget >= 0:
+                    new_key = (remaining_budget, time_cnt + visit_time)
+                    new_max_sum[new_key] = max(new_max_sum[new_key],
+                                               score_sum + score)
             max_sum.update(new_max_sum)
-        return max_sum_of_all
+        return max(max_sum.values())
 
 
 B, P, N = read_line_to_list()
-ATTR = [map(int, read_line_to_list(None)[1:]) for _ in range(N)]
+ATTR = [read_line_to_list(start=1) for _ in range(N)]
 
 planner = TripPlanner(B, P, N, ATTR)
 print planner.max_score_sum()
