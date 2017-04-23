2746bfdce2f70f7c7ae78da9679fab20f0610fae
Alice Wang <rainingilove@gmail.com>
2017-04-23 17:17:42 +0200

    adjust solution 1

--- puzzle/booking/highlight.py
+++ puzzle/booking/highlight.py
@@ -2,8 +2,9 @@ text = raw_input().strip().split(' ')
 num = int(raw_input())
 keywords = set(raw_input().strip().split(' '))
 
-for i in range(len(text)):
-    if text[i] in keywords:
-        text[i] = "<b>{}</b>".format(text[i])
 
-print ' '.join(text)
+for word in text:
+    if word in keywords:
+        print "<b>{}</b>".format(word),
+    else:
+        print word,
