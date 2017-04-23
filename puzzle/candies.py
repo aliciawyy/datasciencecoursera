4d771d0313ca8988588017df4b1b9f0a4341e339
Alice Wang <rainingilove@gmail.com>
2017-04-23 20:47:29 +0200

    improved solution for candies

--- puzzle/candies.py
+++ puzzle/candies.py
@@ -1,52 +1,30 @@
+# python candies.py < data/candies1.in
 
 
-class Candies(object):
-    def __init__(self, arr, n):
-        self.arr = arr
-        self.n = n
-        self.num_ = [1] * self.n
-        # If j-th item is True, it means rating from j - 1 to j is decreasing
-        self.decreasing_ = [False] * self.n
-        self.k_ = 1
-        self._init()
+def count_increasing(ratings, n):
+    """
+    Only considering the increasing case
+    """
+    arr = [1] * n
+    cnt = 1
+    for i in range(1, n):
+        cnt = cnt + 1 if ratings[i - 1] < ratings[i] else 1
+        arr[i] = cnt
+    return arr
 
-    def _init(self):
-        in_init_range = True
-        for i in range(1, self.n):
-            if self.arr[i] < self.arr[i - 1]:
-                self.decreasing_[i] = True
-            elif in_init_range:
-                self.k_ = i
-                in_init_range = False
 
-        if in_init_range:
-            self.k_ = self.n
-
-        for i in range(0, self.k_):
-            self.num_[i] = self.k_ - i
-
-    def go_back(self, k):
-        # Ensure that the candies till k are correct
-        for j in range(k - 1, -1, -1):
-            if self.decreasing_[j+1] and self.num_[j] == self.num_[j+1]:
-                self.num_[j] += 1
-            else:
-                break
-
-    def min_sum(self):
-        for i in range(self.k_, self.n):
-            if self.arr[i] > self.arr[i - 1]:
-                self.num_[i] = self.num_[i - 1] + 1
-            elif self.decreasing_[i] and self.num_[i - 1] == 1:
-                self.go_back(i)
-        return sum(self.num_)
+def min_sum(ratings, n):
+    increase_cnt = count_increasing(ratings, n)
+    ratings.reverse()
+    decrease_cnt = count_increasing(ratings, n)
+    decrease_cnt.reverse()
+    return sum(map(max, zip(increase_cnt, decrease_cnt)))
 
 
 def solve():
     n = int(raw_input())
-    arr = [int(raw_input()) for _ in range(n)]
-    candy = Candies(arr, n)
-    print candy.min_sum()
+    ratings = [int(raw_input()) for _ in range(n)]
+    print min_sum(ratings, n)
 
 
 solve()
